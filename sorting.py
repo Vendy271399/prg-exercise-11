782f43107731ab3d80a04dce6ea943269b1d039e
Vendy271399 <271399@vutbr.cz>
2026-04-20 12:56:18 +0200

    version_02

--- sorting.py
+++ sorting.py
@@ -12,4 +12,30 @@ small = random_numbers(5, low=0, high=20)  # 5 čísel v rozsahu 0–20
 print(small)
 
 
+def selection_sort(values):
+
+    for min_index in range(len(values)):
+        print(values)
+        min_value = values[min_index]
+        min_ind = min_index
+
+        for i in range(min_index, len(values)):
+            if values[i] < min_value:
+                min_ind = i
+                min_value = values[i]
+        values[min_ind], values[min_index] = values[min_index], values[min_ind]
+        print(values)
+
+        return values
+
+
+if __name__ == "__main__":
+    result = selection_sort(small)
+    print(result)
+
+
+
+
+
+
 
